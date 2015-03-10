01fda32a7bfe939ce106d77f096c06c10033cb91
Aleksey Politov <apolitov@eraser.org>
2015-03-11 00:31:19 +0300

    End Of Day 1

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+var toDoList = function($q) {
+
+	var link = function(scope, element, attrs) {
+		element.text("toDoList directive");
+	};
+
+	return {
+		restrict: 'AE',
+		scope: false,
+		link: link,
+
+	};
+}
+
+toDoList.$inject = ["$q"];
+
+angular.module('TodoApp', [])
+	.directive("todoList", toDoList);
